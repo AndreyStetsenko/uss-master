f92d1de25db3b184dce39acab4e2265a3209d2c0
Andrew Stetsenko <andrey@stetsenko.org>
2020-08-23 22:28:32 +0400

    Pagination

--- template-parts/category-gallery.php
+++ template-parts/category-gallery.php
@@ -71,11 +71,23 @@
       		<?php endif;?>
 
         </div>
-        <?php
-        the_posts_pagination( array(
+        <div class="pagination-list">
+          <?php
+          echo paginate_links( array(
+            'show_all'     => false, // показаны все страницы участвующие в пагинации
+          	'end_size'     => 1,     // количество страниц на концах
+          	'mid_size'     => 1,     // количество страниц вокруг текущей
+          	'prev_next'    => true,  // выводить ли боковые ссылки "предыдущая/следующая страница".
+          	'prev_text'    => __('<i class="fas fa-chevron-left"></i>'),
+          	'next_text'    => __('<i class="fas fa-chevron-right"></i>'),
+          	'add_args'     => false, // Массив аргументов (переменных запроса), которые нужно добавить к ссылкам.
+          	'add_fragment' => '',     // Текст который добавиться ко всем ссылкам.
+          	'screen_reader_text' => __( 'Posts navigation' ),
+          ) );
 
-        ) );
-        ?>
+          wp_reset_postdata(); // чтобы ничего не поломать
+          ?>
+        </div>
       </div>
       <div class="col-12 col-md-3">
         <?php get_sidebar(); ?>
